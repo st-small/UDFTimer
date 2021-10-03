a3b125ff51b532ee21b8508a246f9af943e77b88
Stanislav Shiyanovskiy <shiyanovskiy.s@richpeach.pro>
2021-10-03 16:41:48 +0300

    Initial Commit

---
+++ UDFTimerSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UDFTimerSample
+//
+//  Created by Stanly Shiyanovskiy on 03.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
